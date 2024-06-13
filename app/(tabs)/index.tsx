68aa21276b8f55013044255b6856250ce2109d77
Meek Msaki <98189596+mmsaki@users.noreply.github.com>
2024-06-13 16:42:27 -0500

    center view containter

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,9 +1,19 @@
-import { Heading, View } from 'tamagui'
+import { Heading, View, Button, Text } from "tamagui";
 
 export default function HomeScreen() {
+  const results = {
+    address: "0x0001",
+    message: "Hello, 👋🏾",
+  };
   return (
-    <View margin={10}>
+    <View margin={10} style={{ display: "flex", textAlign: "center", justifyContent: "center", alignItems: "center", height: "100%" }}>
       <Heading>Expo Sandbox</Heading>
+      <View style={{ display: "flex", gap: "16px" }}>
+        <Button theme='active'>Sign Message with EOA</Button>
+        <Text>address: {results.address}</Text>
+        <Text>message: {results.message}</Text>
+        <Button theme='active'>Sign Message with Passkey</Button>
+      </View>
     </View>
-  )
+  );
 }
