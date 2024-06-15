2ade3ca4b92ea9bfaf25783a647d302a3664c52e
Meek Msaki <98189596+mmsaki@users.noreply.github.com>
2024-06-14 20:56:38 -0500

    get deterministic address

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,14 +1,14 @@
 import React, { useEffect, useState } from "react";
 import { Heading, View, Button, Text } from "tamagui";
 import { createPublicClient, http, formatEther, Address } from "viem";
-import { mainnet } from "viem/chains";
+import { optimismSepolia } from "viem/chains";
 import { CoinbaseSmartWalletFactoryAbi } from "../abi/CoinbaseSmartWalletFactory";
 import { privateKeyToAccount } from "viem/accounts";
 
-import { domain, types } from "./data";
+import { domain, types, FACTORY_ADDRESS } from "./data";
 
 const publicClient = createPublicClient({
-  chain: mainnet,
+  chain: optimismSepolia,
   transport: http(),
 });
 
@@ -17,7 +17,7 @@ export default function HomeScreen() {
   const [gasPrice, setGasPrice] = useState(0n);
   const [getAddress, setGetAddress] = useState();
   const [signature, setSignature] = useState();
-  const [isValidSignature, SetIsValidSignature] = useState();
+  const [isValidSignature, setIsValidSignature] = useState();
 
   if (!process.env.EXPO_PUBLIC_PRIVATE_KEY) throw Error("Private key not set in .env");
   const account = privateKeyToAccount(`0x${process.env.EXPO_PUBLIC_PRIVATE_KEY}`);
@@ -41,7 +41,7 @@ export default function HomeScreen() {
     });
     // @ts-ignore
     setSignature(sig);
-    console.log(sig);
+
     const valid = await publicClient.verifyTypedData({
       address: "0xf39fd6e51aad88f6f4ce6ab8827279cfffb92266",
       domain: domain,
@@ -61,8 +61,24 @@ export default function HomeScreen() {
       signature: sig,
     });
     // @ts-ignore
-    SetIsValidSignature(valid);
+    setIsValidSignature(valid);
   };
+
+  const getDeterministicAddress = async () => {
+    const data = await publicClient.readContract({
+      address: FACTORY_ADDRESS,
+      abi: CoinbaseSmartWalletFactoryAbi,
+      functionName: "getAddress",
+      args: [["0x000000000000000000000000a5cc3c03994db5b0d9a5eedd10cabab0813678ac"], 0],
+    });
+    // @ts-ignore
+    setGetAddress(String(data));
+  };
+
+  useEffect(() => {
+    getDeterministicAddress();
+    signAndVerifyTypedData();
+  }, [signature, getAddress, isValidSignature]);
 
   return (
     <View margin={10} style={{ display: "flex", textAlign: "center", justifyContent: "center", alignItems: "center", height: "100%" }}>
@@ -71,8 +87,9 @@ export default function HomeScreen() {
           Sign Message with EOA
         </Button>
         <Text>Account: {String(account.address)}</Text>
-        <Text>isValidSignature: {String(isValidSignature)}</Text>
         <Text>Sig: {String(signature)}</Text>
+        <Text>isValidSignature: {String(isValidSignature)}</Text>
+        <Text>Deterministic Address: {getAddress}</Text>
 
         <Button theme='active'>Sign Message with Passkey</Button>
       </View>
