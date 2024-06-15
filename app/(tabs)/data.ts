0bae5a3f1ad04a922f02cf2a8a4be5c71c36ceba
Meek Msaki <98189596+mmsaki@users.noreply.github.com>
2024-06-14 21:20:22 -0500

    update message with own constant

--- app/(tabs)/data.ts
+++ app/(tabs)/data.ts
@@ -19,6 +19,18 @@ export const types = {
   ],
 } as const;
 
+export const message = {
+  from: {
+    name: "Cow",
+    wallet: "0xCD2a3d9F938E13CD947Ec05AbC7FE734Df8DD826",
+  },
+  to: {
+    name: "Bob",
+    wallet: "0xbBbBBBBbbBBBbbbBbbBbbbbBBbBbbbbBbBbbBBbB",
+  },
+  contents: "Hello, Bob!",
+};
+
 export const FACTORY_ADDRESS = "0xabc14A381ab1BC4750eb08D11E5e29506e68c1b9";
 export const MOCK_VERIFIER_ADDRESS = "0x76379783717d3aBA4da7A712C5996cB9Fe468F03";
 export const IMPLEMENTATION_ADDRESS = "0x66Fc5534b1A5521dCA263DCDF44a315eFDAa33C2";
--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,11 +1,11 @@
 import React, { useEffect, useState } from "react";
-import { Heading, View, Button, Text } from "tamagui";
+import { View, Button, Text } from "tamagui";
 import { createPublicClient, http, formatEther, Address } from "viem";
 import { optimismSepolia } from "viem/chains";
 import { CoinbaseSmartWalletFactoryAbi } from "../abi/CoinbaseSmartWalletFactory";
 import { privateKeyToAccount } from "viem/accounts";
 
-import { domain, types, FACTORY_ADDRESS } from "./data";
+import { domain, types, FACTORY_ADDRESS, message } from "./data";
 
 const publicClient = createPublicClient({
   chain: optimismSepolia,
@@ -13,8 +13,6 @@ const publicClient = createPublicClient({
 });
 
 export default function HomeScreen() {
-  const [blockNumber, setBlockNumber] = useState(0n);
-  const [gasPrice, setGasPrice] = useState(0n);
   const [getAddress, setGetAddress] = useState();
   const [signature, setSignature] = useState();
   const [isValidSignature, setIsValidSignature] = useState();
@@ -22,46 +20,34 @@ export default function HomeScreen() {
   if (!process.env.EXPO_PUBLIC_PRIVATE_KEY) throw Error("Private key not set in .env");
   const account = privateKeyToAccount(`0x${process.env.EXPO_PUBLIC_PRIVATE_KEY}`);
 
-  const signAndVerifyTypedData = async () => {
+  const signAndVerifyTypedDataEOA = async () => {
     const sig = await account.signTypedData({
       domain,
-      types: types,
+      types,
       primaryType: "Mail",
-      message: {
-        from: {
-          name: "Cow",
-          wallet: "0xCD2a3d9F938E13CD947Ec05AbC7FE734Df8DD826",
-        },
-        to: {
-          name: "Bob",
-          wallet: "0xbBbBBBBbbBBBbbbBbbBbbbbBBbBbbbbBbBbbBBbB",
-        },
-        contents: "Hello, Bob!",
-      },
+      // @ts-ignore
+      message,
     });
     // @ts-ignore
     setSignature(sig);
 
     const valid = await publicClient.verifyTypedData({
-      address: "0xf39fd6e51aad88f6f4ce6ab8827279cfffb92266",
+      address: account.address,
       domain: domain,
       types: types,
       primaryType: "Mail",
-      message: {
-        from: {
-          name: "Cow",
-          wallet: "0xCD2a3d9F938E13CD947Ec05AbC7FE734Df8DD826",
-        },
-        to: {
-          name: "Bob",
-          wallet: "0xbBbBBBBbbBBBbbbBbbBbbbbBBbBbbbbBbBbbBBbB",
-        },
-        contents: "Hello, Bob!",
-      },
+      message: message,
       signature: sig,
     });
     // @ts-ignore
     setIsValidSignature(valid);
+
+    const data = await publicClient.readContract({
+      address: FACTORY_ADDRESS,
+      abi: CoinbaseSmartWalletFactoryAbi,
+      functionName: "getAddress",
+      args: [["0x000000000000000000000000a5cc3c03994db5b0d9a5eedd10cabab0813678ac"], 0],
+    });
   };
 
   const getDeterministicAddress = async () => {
@@ -72,18 +58,29 @@ export default function HomeScreen() {
       args: [["0x000000000000000000000000a5cc3c03994db5b0d9a5eedd10cabab0813678ac"], 0],
     });
     // @ts-ignore
-    setGetAddress(String(data));
+    setGetAddress(data);
+  };
+
+  const signAndVerifyTypedData6492 = async () => {
+    const data = await publicClient.readContract({
+      address: FACTORY_ADDRESS,
+      abi: CoinbaseSmartWalletFactoryAbi,
+      functionName: "getAddress",
+      args: [["0x000000000000000000000000a5cc3c03994db5b0d9a5eedd10cabab0813678ac"], 0],
+    });
+    // @ts-ignore
+    setGetAddress(data);
   };
 
   useEffect(() => {
     getDeterministicAddress();
-    signAndVerifyTypedData();
+    signAndVerifyTypedDataEOA();
   }, [signature, getAddress, isValidSignature]);
 
   return (
     <View margin={10} style={{ display: "flex", textAlign: "center", justifyContent: "center", alignItems: "center", height: "100%" }}>
       <View style={{ display: "flex", gap: "16px" }}>
-        <Button onClick={() => signAndVerifyTypedData()} theme='active'>
+        <Button onClick={() => signAndVerifyTypedDataEOA()} theme='active'>
           Sign Message with EOA
         </Button>
         <Text>Account: {String(account.address)}</Text>
